9350b1c49f3ae12e43c5352d718578e38e5c5217
Ngeru John <maiguajohn001@gmail.com>
2025-07-29 12:08:02 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,17 @@
+#Simple Calculator
+num1 = float(input("Enter the First Number:"))
+num2 = float(input("Enter the Second Number:"))
+ #Operations
+sum = num1+num2
+difference = num1-num2
+product = num1*num2
+division = num1/num2
+
+#Displaying the results
+print(f"sum: {sum}")
+print(f"difference: {difference}")
+print(f"product: {product}")
+print(f"division: {division}")
+
+
+
