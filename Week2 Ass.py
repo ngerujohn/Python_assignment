6766d43f5ff3105685008dca6211b6f2a469fe18
Ngeru John <maiguajohn001@gmail.com>
2025-08-07 11:53:58 +0300

    Week2 Assignment

---
+++ Week2 Ass.py
@@ -0,0 +1,21 @@
+#create empty list
+my_list=[]
+#append
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+#insert value 15
+my_list.insert(2,15)
+#extend my_list
+my_list.extend([50,60,70])
+#pop
+my_list.pop()
+#sort
+my_list.sort()
+print(my_list)
+#find and print
+index_of_30=my_list.index(30)
+print("index of 30",index_of_30)
+
+
